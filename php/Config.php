8451c7cebede3f54a16a6b3f235d37f8f3e2f263
liviasab <liviasabino92@gmail.com>
2025-03-11 14:38:46 -0300

    feat: credentials

---
+++ php/Config.php
@@ -0,0 +1,8 @@
+<?php
+class ConfiguracaoBanco {
+    const HOST = 'localhost';
+    const USUARIO = 'sys_usuarios_user'; 
+    const SENHA = 'S3nh@S3gur@2024!'; 
+    const BANCO = 'sistema_usuarios_prod';
+}
+?>
